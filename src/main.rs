892af73864dc5c659b8a09fc59b24816c1afb146
Victor van den Elzen <victor.vde@gmail.com>
2022-07-09 10:35:18 +0200

    clean up &

--- src/main.rs
+++ src/main.rs
@@ -22,26 +22,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("colors\tmicroseconds\tname\tpath");
     for path in paths {
-        let graph = match VecVecGraph::from_dimacs_file(&path) {
+        let graph = &match VecVecGraph::from_dimacs_file(&path) {
             Ok(x) => x,
             Err(e) => {
                 eprintln!("Error loading {:?}: {}, Skipping.", path, e);
                 continue;
             }
         };
-        let csr = CsrGraph::from(&graph);
+        let csr = &CsrGraph::from(graph);
         for i in 0..8 {
             use std::time::Instant;
             let now = Instant::now();
             let (name, coloring) = match i {
-                0 => ("color_greedy_naive", color_greedy_naive(&graph)),
-                1 => ("color_greedy_naive csr", color_greedy_naive(&csr)),
-                2 => ("color_greedy_by_degree", color_greedy_by_degree(&graph)),
-                3 => ("color_greedy_by_degree csr", color_greedy_by_degree(&csr)),
-                4 => ("color_greedy_dsatur", color_greedy_dsatur(&graph)),
-                5 => ("color_greedy_dsatur csr", color_greedy_dsatur(&csr)),
-                6 => ("color_greedy_rlf", color_greedy_rlf(&graph)),
-                7 => ("color_greedy_rlf csr", color_greedy_rlf(&csr)),
+                0 => ("color_greedy_naive", color_greedy_naive(graph)),
+                1 => ("color_greedy_naive csr", color_greedy_naive(csr)),
+                2 => ("color_greedy_by_degree", color_greedy_by_degree(graph)),
+                3 => ("color_greedy_by_degree csr", color_greedy_by_degree(csr)),
+                4 => ("color_greedy_dsatur", color_greedy_dsatur(graph)),
+                5 => ("color_greedy_dsatur csr", color_greedy_dsatur(csr)),
+                6 => ("color_greedy_rlf", color_greedy_rlf(graph)),
+                7 => ("color_greedy_rlf csr", color_greedy_rlf(csr)),
                 _ => unreachable!(),
             };
             let elapsed = now.elapsed();
@@ -52,7 +52,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 name,
                 path.file_name().unwrap().to_str().unwrap(),
             );
-            validate_coloring(&graph, &coloring);
+            validate_coloring(graph, &coloring);
         }
     }
 
